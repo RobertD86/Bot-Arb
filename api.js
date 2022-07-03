41b6e9c2a4a10a86ba7a33c8a1da372cd330add4
Robert Dias <frobertdias@gmail.com>
2022-07-02 22:33:02 -0300

    Finalizando a api.js

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 const querystring = require('querystring')
 const crypto = require('crypto')
+const { isTypedArray } = require('util/types')
 
 const apiKey = process.env.API_KEY
 const apiSecret = process.env.SECRET_KEY
@@ -27,3 +28,54 @@ async function privateCall(path, data={}, method='GET'){
         console.log(err)
     }
 }
+
+async function newOrderBuy(symbol, quantity, price, side = 'BUY', type = 'MARKET'){
+    const data = {symbol, side, type, quantity}
+
+    if(price) data.price = price
+    if(type === 'LIMIT') data.timeInForece = 'GTC'
+
+    return privateCall('/v3/order', data, 'POST')
+}
+
+async function newOrderSell(symbol, quantity, price, side = 'SELL', type = 'MARKET'){
+    const data  = {symbol, side, type, quantity}
+
+    if(price) data.price = price
+    if(type === 'LIMIT') data.timeInForece = 'GTC'
+
+    return privateCall('/v3/order', data, 'POST')
+}
+
+async function accountInfo(){
+    return privateCall('/v3/account')
+}
+
+async function publicCall(path, data, method= 'GET'){
+    try{
+        const qs = data ? `?${querystring.stringify(data)}` : ''
+        const result = await axios({
+            method,
+            url: `${apiUrl}${path}${qs}`
+        })
+        return result.data
+
+    }catch(err){
+        console.log(err)
+    }
+
+}
+
+async function time(){
+    return publicCall('/v3/time')
+}
+
+async function depth(symbol = 'BTCBUSD', limit= 5){
+    return publicCall('/v3/depth',{symbol, limit})
+}
+
+async function exchangeInfo(){
+    return publicCall('/v3/exchangeInfo')
+}
+
+module.exports = { time, depth, exchangeInfo, accountInfo, newOrderBuy, newOrderSell}
