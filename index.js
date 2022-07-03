1311235b3032eac26052779e27af56c8dcb1e913
Robert Dias <frobertdias@gmail.com>
2022-07-02 22:55:56 -0300

    Atualizando a função getBuyBuySell

--- index.js
+++ index.js
@@ -18,3 +18,36 @@ ws.onmessage = async(event) => {
     const obj = JSON.parse(event.data)
     BOOK[obg.s] = {ask: parseFloat(obj.a), bid: parseFloat(obj.b)}
 }
+
+async function exchangeInfo(){
+    const response = await axios.get("https://testnet.binance.vision/api/v3/exchangeInfo")
+    const symbols = response.data.symbols.filter(s => s.status === 'TRADING')
+    return symbols.map(s => {
+        return {
+            symbol: s.symbol,
+            base: s.baseAsset,
+            quote: s.quoteAsset
+
+        }
+    })
+}
+
+function getBuyBuySell(buySymbols, allSymbols){
+    const buyBuySell = []
+
+    for(let i = 0; i < buySymbols.length; i++){
+        const buy1 = buySymbols[i]
+
+        const right = allSymbols.filter(s => s.quote === buy1.base)
+
+        for(let j = 0; j < right.length; j++){
+            const buy2 = right[j]
+
+            const sell1 = allSymbols.find(s => s.base === buy2.base && s.quote === buy1.quote)
+            if(!sell1) continue
+
+            buyBuySell.push({buy1, buy2, sell1})
+        }
+    }
+    return buyBuySell
+}
